22b7980bc1ca151818352ab8eb4452e1d6ca2b7c
Pamela Badermann <badermann.pamela@gmail.com>
2025-02-01 15:21:56 -0300

    excecoes explicitas e implicitas

--- ExemplosAula/src/main/java/com/mycompany/exemplosaula/ExemplosAula.java
+++ ExemplosAula/src/main/java/com/mycompany/exemplosaula/ExemplosAula.java
@@ -2,17 +2,14 @@ package com.mycompany.exemplosaula;
 
 public class ExemplosAula {
 
-    private static Derivada ref;
-
-    public static void main (String args[]) {
-        ref = new Derivada ( ); //instancia objeto do tipo Derivada
-        System.out.println ( "=> Imprime o valor de var_base");
-        ref.imprimirVarBase();
-        System.out.println ( "=> Atualiza o valor de var_der com downcasting (var_der = 1000)");
-        ref.atualizarVarSub(1000);
-        System.out.println ( "=> Imprime o valor de var_der com downcasting");
-        ref.imprimirVarSub();
-        System.out.println ( "=> Imprime o valor de var_der");
-        ref.imprimirVarDer();
+    public static void main(String[] args) {
+        int quociente, dividendo=10, divisor=0;
+        try {
+            if (divisor == 0)
+                throw new ArithmeticException("O divisor é zero.");
+            quociente = dividendo / divisor;
+        } catch(Exception e) {
+            System.out.println("ERRO: Divisão por zero: "+e.getMessage());
+        }
     }
 }
--- ExemplosAula/src/main/java/com/mycompany/exemplosaula/TratamentoExcecao.java
+++ ExemplosAula/src/main/java/com/mycompany/exemplosaula/TratamentoExcecao.java
@@ -1,5 +1,26 @@
 package com.mycompany.exemplosaula;
 
 public class TratamentoExcecao {
+//    EXCEÇÃO IMPLICITA
+//    public static void main(String[], args) {
+//        int quociente, dividendo=10, divisor=0;
+//        try {
+//            if (divisor == 0)
+//                throw new ArithmeticException("O divisor é zero.");
+//            quociente = dividendo / divisor;
+//        } catch(Exception e) {
+//            System.out.println("ERRO: Divisão por zero: "+e.getMessage());
+//        }
+//    }
     
+//    EXCEÇÃO EXPLICITA
+    public static int getElemento(int i, int [] vet) {
+        try {
+            if (i < 0 || i > 3)
+            throw new IllegalAccessException  ();
+        } catch ( Exception e ) {
+            System.out.println("ERRO: indice fora dos limites do vetor.");
+        }
+        return vet[i];
+    }
 }
