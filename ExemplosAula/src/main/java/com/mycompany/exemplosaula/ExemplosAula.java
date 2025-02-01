62e4ca35f3ee0759ac7ce0ecc1d3d0ba25c37322
Pamela Badermann <badermann.pamela@gmail.com>
2025-02-01 16:43:36 -0300

    captura e o tratamento da exceção no caso de divisão por zero.

--- ExemplosAula/src/main/java/com/mycompany/exemplosaula/ExemplosAula.java
+++ ExemplosAula/src/main/java/com/mycompany/exemplosaula/ExemplosAula.java
@@ -1,15 +1,45 @@
 package com.mycompany.exemplosaula;
 
-public class ExemplosAula {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        int quociente, dividendo=10, divisor=0;
-        try {
-            if (divisor == 0)
-                throw new ArithmeticException("O divisor é zero.");
-            quociente = dividendo / divisor;
-        } catch(Exception e) {
-            System.out.println("ERRO: Divisão por zero: "+e.getMessage());
-        }
-    }
-}
+    public class ExemplosAula {
+        public static void main ( String args [ ] ) throws InterruptedException {
+            
+            int divisor , dividendo , quociente = 0;
+            String controle = "s";
+	        
+            Scanner s = new Scanner ( System.in );
+            do {    
+                System.out.println ( "Entre com o dividendo." );
+                dividendo = s.nextInt();
+                System.out.println ( "Entre com o divisor." );
+                divisor = s.nextInt();
+
+                try {
+                    quociente = dividendo / divisor;
+                } catch (Exception e)
+                       { 
+                    System.out.println( "ERRO: Divisão por zero!" );
+                }
+
+                System.out.println ( "O quociente é: " + quociente );
+                System.out.println ( "Repetir?" );
+                controle = s.next().toString();
+            } while ( controle.equals( "s" ) );
+            s.close();
+    }    
+} 
+//
+//public class ExemplosAula {
+//
+//    public static void main(String[] args) {
+//        int quociente, dividendo=10, divisor=0;
+//        try {
+//            if (divisor == 0)
+//                throw new ArithmeticException("O divisor é zero.");
+//            quociente = dividendo / divisor;
+//        } catch(Exception e) {
+//            System.out.println("ERRO: Divisão por zero: "+e.getMessage());
+//        }
+//    }
+//}
